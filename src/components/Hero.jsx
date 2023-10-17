f302e8dcb925b757b851f6a1e82f5f55266d98f3
Nuzril <muhammadnuzril2325@gmail.com>
2023-10-17 21:42:39 +0700

    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,20 +3,20 @@ import hero from "../assets/images/hero.png";
 const Hero = () => {
   const social_media = [
     {
-      logo : "logo-instagram",
-      link : "https://www.instagram.com/mnuzrili/"
+      logo: "logo-instagram",
+      link: "https://www.instagram.com/mnuzrili/",
     },
     {
-      logo : "logo-facebook",
-      link : "https://www.facebook.com/mnuzrili"
+      logo: "logo-facebook",
+      link: "https://www.facebook.com/mnuzrili",
     },
     {
-      logo : "logo-linkedin",
-      link : "https://www.linkedin.com/in/muhammad-nuzril-isro-66567a263/"
+      logo: "logo-linkedin",
+      link: "https://www.linkedin.com/in/muhammad-nuzril-isro-66567a263/",
     },
     {
-      logo : "logo-github",
-      link : "https://github.com/mnuzrili"
+      logo: "logo-github",
+      link: "https://github.com/mnuzrili",
     },
   ];
   return (
@@ -34,13 +34,13 @@ const Hero = () => {
               Hello!
               <br />
             </span>
-            My Name is <span>Nuzril</span>
+            I'm <span>Nuzril</span>
           </h1>
           <h4 className="md:text-2xl text-lg md:leading-normal leading-5 mt-4 font-bold text-gray-600">
             Fullstack Developer, Banking IT
           </h4>
           <a href="#contact">
-            <button  className="btn btn-primary mt-8">Contact Me</button>
+            <button className="btn btn-primary mt-8">Contact Me</button>
           </a>
           <div className="mt-8 text-3xl flex items-center md:justify-start justify-center gap-5">
             {social_media?.map((x) => (
